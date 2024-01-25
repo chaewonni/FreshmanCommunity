edc04c5560be51ddbdd459e1fd50d9cd9bf21a38
chaewonni <kimlove9182@gmail.com>
2024-01-25 17:25:09 +0900

    first commit

---
+++ server/freshmanCommunity/src/main/java/teamFive/freshmanCommunity/FreshmanCommunityApplication.java
@@ -0,0 +1,13 @@
+package teamFive.freshmanCommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshmanCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshmanCommunityApplication.class, args);
+	}
+
+}
---
+++ server/freshmanCommunity/src/test/java/teamFive/freshmanCommunity/FreshmanCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package teamFive.freshmanCommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshmanCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
