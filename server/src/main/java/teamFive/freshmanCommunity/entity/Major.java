e18492ddf87d02de5d332ea8a1b6cfaca3c86351
cakenpeace <chwon02@naver.com>
2024-02-08 00:25:41 +0900

    feat: 기능 구현 위한 임시 멤버, 메이저 엔티티 생성

---
+++ server/src/main/java/teamFive/freshmanCommunity/entity/Major.java
@@ -0,0 +1,10 @@
+package teamFive.freshmanCommunity.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Major {
+    @Id
+    private Long id;
+}
---
+++ server/src/main/java/teamFive/freshmanCommunity/entity/Member.java
@@ -0,0 +1,10 @@
+package teamFive.freshmanCommunity.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    private Long id;
+}
