b8688d348c8023bdb64064d60d4131492c23730c
cakenpeace <chwon02@naver.com>
2024-02-08 12:13:57 +0900

    feat: 레포지토리에 전체 게시글 조회 메소드 구현

--- server/src/main/java/teamFive/freshmanCommunity/repository/ArticleRepository.java
+++ server/src/main/java/teamFive/freshmanCommunity/repository/ArticleRepository.java
@@ -1,8 +1,11 @@
 package teamFive.freshmanCommunity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import teamFive.freshmanCommunity.entity.Article;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-    Article findArticleById(Long articleId);
+    @Query(value = "SELECT * FROM article",
+        nativeQuery = true)
+    Article showAll();
 }
