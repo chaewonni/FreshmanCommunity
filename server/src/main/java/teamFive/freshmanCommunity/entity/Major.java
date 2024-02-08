379ae3ad388f4c292d1e09fed2e69d017a82b92c
cakenpeace <chwon02@naver.com>
2024-02-08 15:51:57 +0900

    refactor: 임시 클래스에 getter 추가 (conflict 막기 위해 머지 제외 필요)

--- server/src/main/java/teamFive/freshmanCommunity/entity/Major.java
+++ server/src/main/java/teamFive/freshmanCommunity/entity/Major.java
@@ -2,8 +2,10 @@ package teamFive.freshmanCommunity.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Major {
     @Id
     private Long id;
--- server/src/main/java/teamFive/freshmanCommunity/entity/Member.java
+++ server/src/main/java/teamFive/freshmanCommunity/entity/Member.java
@@ -2,8 +2,10 @@ package teamFive.freshmanCommunity.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
 
 @Entity
+@Getter
 public class Member {
     @Id
     private Long id;
