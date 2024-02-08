b8af1e385b1ff5fb08ef835c9ab6745ce105dd41
cakenpeace <chwon02@naver.com>
2024-02-08 12:06:10 +0900

    refactor: 더미 데이터 삭제

--- server/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO article(title, content, createDate, memberId, majorId) VALUES("안녕", "하세요", 2024-02-08, 1, 1);
-INSERT INTO article(title, content, createDate, memberId, majorId) VALUES("2", "하세용", 2024-02-06, 2, 1);
-INSERT INTO article(title, content, createDate, memberId, majorId) VALUES("3", "하세유", 2024-02-07, 3, 1);
