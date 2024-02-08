8eebd43e7431ebd62dd02b790ac0afab8e2024db
chaewonkim <kimlove9182@gmail.com>
2024-02-08 23:28:17 +0900

    feat: 회원가입 및 로그인 예외처리 로직 추가했습니다.

---
+++ server/src/main/java/teamFive/freshmanCommunity/exception/DuplicateMemberException.java
@@ -0,0 +1,7 @@
+package teamFive.freshmanCommunity.exception;
+
+public class DuplicateMemberException extends RuntimeException{
+    public DuplicateMemberException(String message) {
+        super(message);
+    }
+}
