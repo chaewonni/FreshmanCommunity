b5626ee01c739111ed6e7fe5da675ea67e51aa89
cakenpeace <chwon02@naver.com>
2024-02-09 19:36:36 +0900

    refactor: 핸들러 경로 수정 및 에러 이름 변경

--- server/src/main/java/teamFive/freshmanCommunity/exception/GlobalExceptionHandler.java
+++ server/src/main/java/teamFive/freshmanCommunity/exception/GlobalExceptionHandler.java
@@ -5,14 +5,13 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import teamFive.freshmanCommunity.exception.NonExistentBoardIdException;
 
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
-    @ExceptionHandler(NonExistentBoardIdException.class)
+    @ExceptionHandler(BoardNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String nonExistentBoardIdExceptionHandler(NonExistentBoardIdException e) {
+    public String nonExistentBoardIdExceptionHandler(BoardNotFoundException e) {
         log.error("{}", e.getMessage());
         return e.getMessage();
     }
