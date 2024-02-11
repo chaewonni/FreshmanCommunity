37f9e7f307e91b99a7f77e22df316292631eb181
Seungyeon Han <tiffanyhansy@gmail.com>
2024-02-12 02:25:14 +0900

    docs : 초기 세팅 파일

---
+++ client/.prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true, //문자열에서 큰따옴표 대신 작은따옴표로 바꾸기
+  trailingComma: 'none',
+  arrowParens: 'avoid',
+  semi: false, // 세미콜론이 붙은 문장에서 세미콜론 모두 지우기
+  printWidth: 120
+}
