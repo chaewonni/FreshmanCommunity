c4457460925a3844b6cdb0a1dea40ac985768602
cakenpeace <chwon02@naver.com>
2024-02-14 02:30:01 +0900

    feat: Article 반환 시 사용하는 DTO 추가

---
+++ server/src/main/java/teamFive/freshmanCommunity/dto/ArticleReadDto.java
@@ -0,0 +1,17 @@
+package teamFive.freshmanCommunity.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+public class ArticleReadDto {
+    private Long id;
+    private String title;
+    private String content;
+    private LocalDateTime createDate;
+    private LoginResponseDto loginResponseDto;
+
+}
