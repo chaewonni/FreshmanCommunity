3ae416425a2a8b0be32ec86e21d7e504a4c7763e
chaewonkim <kimlove9182@gmail.com>
2024-02-14 15:18:36 +0900

    feat: LikeCommentApiController 생성 및 댓글 좋아요 로직 추가

---
+++ server/src/main/java/teamFive/freshmanCommunity/api/LikeCommentApiController.java
@@ -0,0 +1,28 @@
+package teamFive.freshmanCommunity.api;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import teamFive.freshmanCommunity.entity.LikeComment;
+import teamFive.freshmanCommunity.service.LikeCommentService;
+
+@RestController
+@RequiredArgsConstructor
+public class LikeCommentApiController {
+
+    private final LikeCommentService likeCommentService;
+
+    @PostMapping("/comment/{commentId}/like")
+    public ResponseEntity<String> addLike(@PathVariable Long commentId, HttpServletRequest request) {
+
+        HttpSession session = request.getSession();
+        LikeComment.addLike(commentId, session);
+        return ResponseEntity.status(HttpStatus.OK).body("좋아요 처리 완료");
+    }
+
+}
