e962c18b79cfcd667fba3f90d9c2429e47f1ce3c
chaewonkim <kimlove9182@gmail.com>
2024-02-15 00:30:26 +0900

    chore: 멤버 id 컬럼명 변경

--- server/src/main/java/teamFive/freshmanCommunity/entity/Member.java
+++ server/src/main/java/teamFive/freshmanCommunity/entity/Member.java
@@ -15,7 +15,7 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "memberId")
+    @Column(name = "member_id")
     private Long id;
 
     @Column
