b3dc714f0f36853fdcd8f417993f89291a09c565
cakenpeace <chwon02@naver.com>
2024-02-14 19:03:04 +0900

    feat: ArticleReadDto에 major 정보 추가

--- server/src/main/java/teamFive/freshmanCommunity/dto/ArticleReadDto.java
+++ server/src/main/java/teamFive/freshmanCommunity/dto/ArticleReadDto.java
@@ -2,6 +2,7 @@ package teamFive.freshmanCommunity.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import teamFive.freshmanCommunity.entity.Major;
 
 import java.time.LocalDateTime;
 
@@ -12,6 +13,8 @@ public class ArticleReadDto {
     private String title;
     private String content;
     private LocalDateTime createDate;
+    // 유저의 메이저 정보를 포함
     private LoginResponseDto memberReadDto;
-
+    // 게시글의 메이저 정보를 포함 (유저의 메이저와 게시글의 메이저가 다를 수 있으므로)
+    private Major boardMajor;
 }
