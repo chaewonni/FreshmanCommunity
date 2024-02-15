bba6e99b025c1a9bd3cee28cda77322441d7e71b
chaewonkim <kimlove9182@gmail.com>
2024-02-16 01:17:50 +0900

    feat: createDate의 역순으로 조회하는 쿼리 메서드 추가

--- server/src/main/java/teamFive/freshmanCommunity/repository/BookmarkRepository.java
+++ server/src/main/java/teamFive/freshmanCommunity/repository/BookmarkRepository.java
@@ -5,9 +5,13 @@ import teamFive.freshmanCommunity.entity.Article;
 import teamFive.freshmanCommunity.entity.Bookmark;
 import teamFive.freshmanCommunity.entity.Member;
 
+import java.util.List;
+
 public interface BookmarkRepository extends JpaRepository<Bookmark, Long> {
 
     Bookmark findByMemberAndArticle(Member member, Article article);
 
     void deleteByMemberAndArticle(Member member, Article article);
+
+    List<Bookmark> findAllByMemberOrderByCreateDateDesc(Member member);
 }
