b645dc30a89f9d6b8f614e6df37e23fd5b84ede1
chaewonkim <kimlove9182@gmail.com>
2024-02-15 18:06:20 +0900

    refactor: 멤버 로직 코드 전체 리팩토링

--- server/src/main/java/teamFive/freshmanCommunity/repository/MemberRepository.java
+++ server/src/main/java/teamFive/freshmanCommunity/repository/MemberRepository.java
@@ -10,13 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    List<Member> findByMemberName(String memberName);
-
-//    List<Member> findByEmail(String email);
-
     Member findByEmail(String email);
 
     Long countByEmail(String email);
-
-    Long countByStudentId(String studentId);
 }
