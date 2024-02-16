661663094421b793439f318f9f86068fa139f78a
Seungyeon Han <tiffanyhansy@gmail.com>
2024-02-17 05:53:14 +0900

    feat: 새 카드 추가 기능 구현

--- client/src/components/modal/Modal.jsx
+++ client/src/components/modal/Modal.jsx
@@ -1,1 +1,27 @@
-export const Modal = () => <div>"모달"</div>;
+import * as React from 'react';
+import Modal from '@mui/material/Modal';
+import Paper from '@mui/material/Paper';
+
+function NewCardModal({ isOpen, closeModal, children }) {
+  return (
+    <Modal open={isOpen} onClose={closeModal}>
+      <Paper
+        elevation={2}
+        sx={{
+          position: 'absolute',
+          top: '50%',
+          left: '50%',
+          transform: 'translate(-50%, -50%)',
+          width: 900,
+          height: 900,
+          maxWidth: '100%',
+          maxHeight: '90%',
+          overflowY: 'auto',
+        }}>
+        {children}
+      </Paper>
+    </Modal>
+  );
+}
+
+export default NewCardModal;
