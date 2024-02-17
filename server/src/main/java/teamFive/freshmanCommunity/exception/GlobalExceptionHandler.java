23b20ef7f60b398c485a113b4bd653fd9fc4a893
cakenpeace <chwon02@naver.com>
2024-02-18 03:47:09 +0900

    feat: 새로운 에러에 대한 핸들러 추가

--- server/src/main/java/teamFive/freshmanCommunity/exception/GlobalExceptionHandler.java
+++ server/src/main/java/teamFive/freshmanCommunity/exception/GlobalExceptionHandler.java
@@ -61,4 +61,18 @@ public class GlobalExceptionHandler {
                 .status(HttpStatus.NOT_FOUND)
                 .body(e.getMessage());
     }
+    @ExceptionHandler(MajorConflictWithArticleException.class)
+    public ResponseEntity<String> handleMajorConflictWithArticleException(MajorConflictWithArticleException e){
+        log.error("{}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(e.getMessage());
+    }
+    @ExceptionHandler(ArticleNotFoundException.class)
+    public ResponseEntity<String> handleArticleNotFoundException(ArticleNotFoundException e){
+        log.error("{}", e.getMessage());
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(e.getMessage());
+    }
 }
