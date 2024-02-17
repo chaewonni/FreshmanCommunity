592d4af9bd681ac75003701324aafa16e4b854e9
cakenpeace <chwon02@naver.com>
2024-02-18 03:47:35 +0900

    feat: 게시글 하나 조회 로직 추가

--- server/src/main/java/teamFive/freshmanCommunity/api/ArticleApiController.java
+++ server/src/main/java/teamFive/freshmanCommunity/api/ArticleApiController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import teamFive.freshmanCommunity.dto.ArticleReadDto;
 import teamFive.freshmanCommunity.dto.ArticleCreateDto;
+import teamFive.freshmanCommunity.exception.ArticleNotFoundException;
+import teamFive.freshmanCommunity.exception.MajorConflictWithArticleException;
 import teamFive.freshmanCommunity.service.ArticleService;
 
 import java.util.List;
@@ -18,17 +20,31 @@ public class ArticleApiController {
     @Autowired
     private ArticleService articleService;
 
-    //1. 게시글 조회
+    //1-1. 게시글 전체 조회
     @GetMapping("/articles/{majorId}")
     public ResponseEntity<List<ArticleReadDto>> showAll(@PathVariable Long majorId) {
         // 서비스에 위임
         List<ArticleReadDto> dtos = articleService.articles(majorId);
         // 예외 처리
         if (dtos == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+            throw new ArticleNotFoundException();
         }
         // 결과 응답
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+
+    //1-2. 게시글 하나 조회
+    @GetMapping("/articles/{majorId}/{articleId}")
+    public ResponseEntity<ArticleReadDto> showOne(@PathVariable Long majorId, @PathVariable Long articleId) {
+        // 서비스에 위임
+        ArticleReadDto dto = articleService.oneArticle(majorId, articleId);
+        // 예외 처리
+        // 1. 불러온 게시글의 전공 id와 조회하는 전공의 id가 다를
+        if (!(dto.getBoardMajor().getId().equals(majorId))) {
+            throw new MajorConflictWithArticleException();
+        }
+        // 결과 응답
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
     }
 
     //2. 게시글 생성
