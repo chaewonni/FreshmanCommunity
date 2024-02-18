c2de810862876b36a602e8d7f74bafd27f5c1181
Bokjihee <bokjihee@naver.com>
2024-02-18 22:03:21 +0900

    feat: 댓글좋아요 10개 이상시 맨 위로 정렬되는 sql문 추가

--- server/src/main/java/teamFive/freshmanCommunity/repository/CommentRepository.java
+++ server/src/main/java/teamFive/freshmanCommunity/repository/CommentRepository.java
@@ -10,4 +10,11 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     //게시글 id로 모든 댓글 찾기
     @Query(value="SELECT * FROM comment WHERE article_id = :articleId", nativeQuery = true)
     List<Comment> findByArticleId(Long articleId);
+
+    //댓글 좋아요 10개 넘으면 맨 위로
+    @Query(value = "SELECT * FROM comment " +
+            "WHERE article_id = :articleId " +
+            "ORDER BY CASE WHEN likes >= 10 THEN 0 ELSE 1 END, likesCount DESC",    //0인 댓글들 먼저 정렬하고
+            nativeQuery = true)
+    List<Comment> findAllOrderByLikes(Long articleId);
 }
