7356a203bdfa666a4983bd8b81d95e19adfe13f8
Seungyeon Han <tiffanyhansy@gmail.com>
2024-03-28 23:41:04 +0900

    📝 Chore(#1): 초기세팅 파일 수정

---
+++ client/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended'
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true }
+    ],
+    'react/react-in-jsx-scope': 'off',
+    curly: 'error',
+    'operator-linebreak': 'off'
+  }
+};
